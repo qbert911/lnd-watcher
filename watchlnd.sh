c9b1f28846e7bdd4e4650f780cd0218ca0bd3785
bongo <yurgartisb@gmail.com>
2019-03-30 18:21:41 -0400

    clean up

--- watchlnd.sh
+++ watchlnd.sh
@@ -36,40 +36,35 @@ while : ;do
   
   sort nodelist.txt -o nodelist.txt
   displaywidth=`tput cols` 
-  if   [ "$displaywidth" -gt 164 ]; then dispsize="A";colorda="007m";colordb="007m";colordc="007m";colordd="007m";colorde="001m"
-  elif [ "$displaywidth" -gt 134 ]; then dispsize="B";colorda="007m";colordb="007m";colordc="007m";colordd="001m";colorde="007m"
-  elif [ "$displaywidth" -gt 104 ]; then dispsize="C";colorda="007m";colordb="007m";colordc="001m";colordd="007m";colorde="007m"
-  elif [ "$displaywidth" -gt 79  ]; then dispsize="D";colorda="007m";colordb="001m";colordc="007m";colordd="007m";colorde="007m"
-  else                                   dispsize="E";colorda="001m";colordb="007m";colordc="007m";colordd="007m";colorde="007m";updatetime=$((5));fi
+  if   [ "$displaywidth" -gt 164 ]; then dispsize="A";colorda="007m";colordb="007m";colordc="007m";colordd="007m";colorde="001m";updatetimed=$updatetime
+  elif [ "$displaywidth" -gt 134 ]; then dispsize="B";colorda="007m";colordb="007m";colordc="007m";colordd="001m";colorde="007m";updatetimed=$updatetime
+  elif [ "$displaywidth" -gt 104 ]; then dispsize="C";colorda="007m";colordb="007m";colordc="001m";colordd="007m";colorde="007m";updatetimed=$updatetime
+  elif [ "$displaywidth" -gt 79  ]; then dispsize="D";colorda="007m";colordb="001m";colordc="007m";colordd="007m";colorde="007m";updatetimed=$updatetime
+  else                                   dispsize="E";colorda="001m";colordb="007m";colordc="007m";colordd="007m";colorde="007m";updatetimed=$((5));fi
   walletbal="             ${walletbal}";walletbalA="${walletbal:(-9):3}";walletbalB="${walletbal:(-6):3}";walletbalC="${walletbal:(-3):3}";walletbal="${walletbalA// /} ${walletbalB// /} ${walletbalC// /}";walletbal="${walletbal/  /}"
 #----------START--WEB DATA GRABBER---------------------------------------------
-  mkdir -p pages;rm -f nodelist-temp.txt pages/webdatanew.txt
-  cp nodelist.txt nodelist-temp.txt
-  myrecs=$(wc -l nodelist-temp.txt | sed -e 's/ .*//')
+  myrecs=$(wc -l nodelist.txt | sed -e 's/ .*//')
+  savedrecs=$(wc -l pages/webdata.txt | sed -e 's/ .*//')
   dirty=false
   while read thisID unused; do
-    if ! test -f "pages/$thisID.html" || test "`find pages/$thisID.html -mmin +30`" || ! test -f "pages/webdata.txt" ;then  #freshness check
-      dirty=true
-    else
-      if [ "$myrecs" != "$(wc -l pages/webdata.txt | sed -e 's/ .*//')" ];then dirty=true;fi
-    fi
-  done < nodelist-temp.txt
+    if ! test -f "pages/$thisID.html" || test "`find pages/$thisID.html -mmin +30`" || ! test -f "pages/webdata.txt" ;then dirty=true;break    #freshness check
+    elif [ "$myrecs" != "$savedrecs" ];then dirty=true;break;fi
+  done < nodelist.txt
   if [ "$dirty" = true ];then
+    mkdir -p pages;rm -f pages/webdata.txt
     echo "Downloading data about $myrecs nodes from 1ml.com : "`date`
     barlen=$(( $displaywidth )) 
     for (( c=1; c<=$(( $barlen - ( $(( $barlen  / $myrecs )) * $myrecs ) )); c++ )); do echo -ne "=";done        #fill in gap bars segments
     while read thisID f2 f3 f4 f5; do
         if ! test -f "pages/$thisID.html" || test "`find pages/$thisID.html -mmin +27`";then  #freshness check
-          eval curl -s https://1ml.com/node/$thisID/channels?order=capacity -o pages/$thisID.html
-          for (( c=1; c<=$(( $barlen  / $myrecs / 2 )); c++ )); do echo -n -e "\e[38;5;54m=\e[0m";done          #draw bar segment
-        else
-          for (( c=1; c<=$(( $barlen  / $myrecs / 2 )); c++ )); do echo -n -e "\e[38;5;235m=\e[0m";done          #draw bar segment
-        fi #download html
+          eval curl -s https://1ml.com/node/$thisID/channels?order=capacity -o pages/$thisID.html   #download html
+          for (( c=1; c<=$(( $barlen  / $myrecs / 2 )); c++ )); do echo -n -e "\e[38;5;54m=\e[0m";done    #draw bar segment
+        else for (( c=1; c<=$(( $barlen  / $myrecs / 2 )); c++ )); do echo -n -e "\e[38;5;235m=\e[0m";done;fi   #draw bar segment
         if eval head -n 200 "pages/$thisID.html" | grep -q 'globe';then
           thisgeodata=`eval head -n 200 pages/$thisID.html|grep -A4 "globe"|pup a,li text{}| tr '\n' ','`
         else thisgeodata=" , , ";fi
           hex=`eval head -n 200 pages/$thisID.html| grep -A1 '<h5>Color</h5>' | pup span text{} | jq -r -R '.[1:7]'`
-          r=$(printf '0x%0.2s' "$hex"); g=$(printf '0x%0.2s' ${hex#??}); b=$(printf '0x%0.2s' ${hex#????})  #hex to anso color conversion
+          r=$(printf '0x%0.2s' "$hex"); g=$(printf '0x%0.2s' ${hex#??}); b=$(printf '0x%0.2s' ${hex#????})  #hex to ansi color conversion
         thiscolor=$(echo -e `printf "%03d" "$(((r<75?0:(r-35)/40)*6*6+(g<75?0:(g-35)/40)*6+(b<75?0:(b-35)/40)+16))"`)"m"
         thiscapacity=`eval head -n 200 pages/$thisID.html|grep -A1 "<h5>Capacity" |pup span text{}`
         thisconnectedcount=`eval head -n 200 pages/$thisID.html|grep -A1 "<h5>Connected Node Count</h5>" |pup span text{}| sed 's/,//'`
@@ -80,31 +75,24 @@ while : ;do
         title=`eval lncli getnodeinfo ${thisID} |jq -r '.node.alias'| tr -d "<)'(>"`    #remove problem characters from alias
         ipexam=`eval lncli getnodeinfo ${thisID} |jq -r '.node.addresses[].addr'`
         ipstatus="-ip4-";ipcolor="089m"
-        if [[ $ipexam == *"n:"* ]];then ipstatus="onion";ipcolor="113m";fi
-        if [[ $ipexam == *":"*":"* ]];then ipstatus="mixed";ipcolor="111m";fi
-        if [[ $ipexam == *"n:"*"n:"* ]];then ipstatus="onion";ipcolor="113m";fi
-        if [[ $ipexam == *":"*":"*":"* ]];then ipstatus="mixed ";ipcolor="111m";fi
-        eval echo "${thisID},${title},${ipstatus},${ipcolor},${thiscapacity:0:-4},${thisconnectedcount},${avgchancap},${thisbiggestchan},${thisage},${thiscolor},${thisgeodata:0:-1}" >> pages/webdatanew.txt  #write line to file
+        if [[ $ipexam == *"n:"* ]];then        ipstatus="onion";ipcolor="113m";fi
+        if [[ $ipexam == *":"*":"* ]];then     ipstatus="mixed";ipcolor="111m";fi
+        if [[ $ipexam == *"n:"*"n:"* ]];then   ipstatus="onion";ipcolor="113m";fi
+        if [[ $ipexam == *":"*":"*":"* ]];then ipstatus="mixed";ipcolor="111m";fi
         for (( c=1; c<=$(( ( $barlen  / $myrecs ) - $(( $barlen  / $myrecs / 2 )) )); c++ )); do echo -ne "\e[38;5;99m=\e[0m";done     #draw bar segment
-    done < nodelist-temp.txt
-    sort pages/webdatanew.txt -o pages/webdata.txt
-    rm -f pages/webdatanew.txt
+        eval echo "${thisID},${title},${ipstatus},${ipcolor},${thiscapacity:0:-4},${thisconnectedcount},${avgchancap},${thisbiggestchan},${thisage},${thiscolor},${thisgeodata:0:-1}" >> pages/webdata.txt  #write line to file
+    done < nodelist.txt
   fi
 #----------START--TABLE BUILDER------------------------------------------------
   rm -f combined.txt     #just in case of program interruption 
-  recs=$((-1))           #so we don't count self
   while read -r thisID balance incoming cstate init cf && read -r thatID title ipstatus ipcolor thiscapacity thisconnectedcount avgchancap thisbiggestchan age color city state country junk <&3; do
-    : $((recs++))
     #--------------processing  	
     if   [ "$state"   = "" ];then country=$city ;              city=""
     elif [ "$country" = "" ];then country=$state; state=$city; city="";fi
-
     if   [ "$init"   = "true" ];then balance=$(( $balance + $cf ))
   	elif [ "$init"   = "false" ];then incoming=$(( $incoming + $cf ));fi
-
   	if [ "$balance"   = "0" ];then balance="";fi
   	if [ "$incoming"  = "0" ];then incoming="";fi
-
     if [[ -n "$incoming" ]];then incoming="          ${incoming}";incomingA="${incoming:(-9):3}";incomingB="${incoming:(-6):3}";incomingC="${incoming:(-3):3}";incoming="${incomingA// /} ${incomingB// /} ${incomingC// /}";incoming="${incoming/  /}";fi
     if [[ -n "$balance" ]];then abalance="           ${balance}";balanceA="${abalance:(-9):3}";balanceB="${abalance:(-6):3}";balanceC="${abalance:(-3):3}";balance="${balanceA// /} ${balanceB// /} ${balanceC// /}";balance="${balance/  /}";fi
     incoming="'\e[38;5;232m'___________'\e[0m'${incoming}";incoming="${incoming:0:14}${incoming: -17}"
@@ -132,9 +120,9 @@ while : ;do
   echo -e "${header}\n`cat combined.txt|sort --field-separator=',' -k 7,7 -k 5,5 -k 4`"  | column -n -ts, > myout.txt  #main data table
   clear;  echo -e `cat myout.txt` #helps with screen refresh lag?
   echo -e "  (${unconfirmed} unconf) (${limbo} in limbo$limbot) (${unset_balanceo} / ${unset_balancei} unsettled ${unset_times}) Recent fwds: ${fwding}"
-  echo -e "In wallet   \e[38;5;45m${walletbal}\e[0m    Income: \e[38;5;83m${income}\e[0m   Chans: \e[38;5;99m${recs}\e[0m (${reco}/${reci})"
-  rm -f combined.txt myout.txt nodelist.txt nodelist-temp.txt rawout.txt rawoutp.txt
-  secsi=$updatetime;while [ $secsi -gt -1 ]; do echo -ne " Columns~"`tput cols`" [\e[38;5;${colorda}50\e[38;5;$colordb 80\e[38;5;$colordc 105\e[38;5;$colordd 135\e[0m and\e[38;5;$colorde 165\e[0m] "
-  for (( c=1; c<=$(( $updatetime - $secsi )); c++ )); do echo -ne " ";done ;  for (( c=1; c<=$(( $secsi )); c++ )); do echo -ne "»";done ;echo -ne " \e[38;5;173m$secsi\e[0m "
+  echo -e "In wallet   \e[38;5;45m${walletbal}\e[0m    Income: \e[38;5;83m${income}\e[0m   Chans: \e[38;5;99m${myrecs}\e[0m (${reco}/${reci})"
+  rm -f combined.txt myout.txt nodelist.txt rawout.txt rawoutp.txt
+  secsi=$updatetimed;while [ $secsi -gt -1 ]; do echo -ne " Columns~"`tput cols`" [\e[38;5;${colorda}50\e[38;5;$colordb 80\e[38;5;$colordc 105\e[38;5;$colordd 135\e[0m and\e[38;5;$colorde 165\e[0m] "
+  for (( c=1; c<=$(( $updatetimed - $secsi )); c++ )); do echo -ne " ";done ;  for (( c=1; c<=$(( $secsi )); c++ )); do echo -ne "»";done ;echo -ne " \e[38;5;173m$secsi\e[0m "
   for (( c=1; c<=$(( $secsi )); c++ )); do echo -ne "«";done ;echo -en "\033[0K\r\e[?25l"; if  [ $secsi -ne 0 ];then sleep 1;fi ; : $((secsi--));done   #countdown
 done
